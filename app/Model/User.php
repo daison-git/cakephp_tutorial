7ff91bc102d2d09bdcf37dd110a8b850a4871345
daison-git <shukatsudaison@gmail.com>
2016-03-08 06:05:15 +0900

    パスワードのハッシュ化

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,5 +1,8 @@
 <?php
 
+App::uses('AppModel', 'Model');
+App::uses('BlowfishPasswordhasher', 'Controller/Component/Auth');
+
 class User extends AppModel {
     public $validate = array(
         'username' => array(
@@ -22,4 +25,15 @@ class User extends AppModel {
                 )
             ),
         );
+
+    public function beforeSave($options = array()) {
+        if (isset($this->data[$this->alias]['password'])) {
+            $passwordHasher = new BlowfishPasswordhasher();
+            $this->data[$this->alias]['password'] = $passwordHasher->hash(
+                $this->data[$this->alias]['password']);
+        };
+
+        return true;
+    }
+
 }
